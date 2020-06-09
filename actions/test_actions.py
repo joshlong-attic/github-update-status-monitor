c8a8efce307c25d27daf014dd42611aa424b969b
Josh Long <josh@joshlong.com>
2020-06-09 04:03:55 -0700

    up

--- actions/test_actions.py
+++ actions/test_actions.py
@@ -1,9 +1,9 @@
-import unittest
-import os
-import pg8000
-import actions
 import datetime
+import unittest
+
+import actions
 import db
+import main
 
 
 class GithubActionsRunServiceTest(unittest.TestCase):
@@ -11,20 +11,11 @@ class GithubActionsRunServiceTest(unittest.TestCase):
     repository = 'simple-python-github-client-test'
 
     def setUp(self) -> None:
-
-        def build_connection() -> pg8000.Connection:
-            db = os.environ.get('UMS_DB_NAME', 'ttd')
-            host = os.environ.get('UMS_DB_HOST', 'localhost')
-            username = os.environ.get('UMS_DB_USERNAME', 'orders')
-            pw = os.environ.get('UMS_DB_PASSWORD', 'orders')
-            return pg8000.connect(username, password=pw, database=db, host=host)
-
         def reset(cursor):
             cursor.execute('drop table if exists github_action_runs')
 
-        db.execute_in_transaction(build_connection, reset)
-
-        self.github_actions_db_service = actions.GithubActionsRunService(build_connection)
+        db.execute_in_transaction(main.build_connection, reset)
+        self.github_actions_db_service = actions.GithubActionsRunService(main.build_connection)
 
     def test_write_run_for(self):
         now = datetime.datetime.now()
@@ -34,5 +25,3 @@ class GithubActionsRunServiceTest(unittest.TestCase):
         id, owner, repo, updated_at = self.github_actions_db_service.read_run_for(self.owner, self.repository)
         print('the id is', id, 'and the owner is', owner, 'and the repository is', repo, 'and it was updated at',
               updated_at)
-
-        # self.assertTrue( resu)
--- main.py
+++ main.py
@@ -16,9 +16,21 @@ import os
 import typing
 
 import dateutil.parser
+import datetime
 import github
 # https://github.com/tlocke/pg8000
 import mappings
+import pg8000
+import actions
+
+
+def build_connection() -> pg8000.Connection:
+    db = os.environ.get('UMS_DB_NAME', 'ttd')
+    host = os.environ.get('UMS_DB_HOST', 'localhost')
+    username = os.environ.get('UMS_DB_USERNAME', 'orders')
+    pw = os.environ.get('UMS_DB_PASSWORD', 'orders')
+    return pg8000.connect(username, password=pw, database=db, host=host)
+
 
 if __name__ == '__main__':
 
@@ -27,31 +39,36 @@ if __name__ == '__main__':
 
 
     github_token = os.environ.get('GITHUB_PERSONAL_ACCESS_TOKEN')
-    db = os.environ.get('UMS_DB_NAME', 'ttd')
-    pw = os.environ.get('UMS_DB_PASSWORD', 'orders')
-    host = os.environ.get('UMS_DB_HOST', 'localhost')
-    username = os.environ.get('UMS_DB_USERNAME', 'orders')
-
     github_client = github.SimpleGithubClient(github_token)
-    actions = github_client.actions()
+    db_service = actions.GithubActionsRunService(build_connection)
 
     with open('event-mappings.json') as json_fp:
         event_mappings = mappings.EventMapping.read_event_mappings(json_fp)
-        print(event_mappings)
-        # foreach mapping:
-        # check to see if there's a recent successful build.
-        # if there is and it's newer than the last successful build
-        # note it in the DB, trigger an event
+
         for em in event_mappings:
-            recent_runs = actions.list_workflow_runs(
+            recent_runs = github_client.actions().list_workflow_runs(
                 owner=em.source.owner,
                 repo=em.source.repository,
                 workflow_file_name_or_id=em.source.workflow_file_name,
                 status='success')['workflow_runs']
             successful_runs = [a for a in recent_runs if a['status'] == 'completed' and a['conclusion'] == 'success']
             successful_runs.sort(key=key_generator, reverse=True)
-
             latest_successful_run = successful_runs[0]
             updated_at = dateutil.parser.parse(latest_successful_run['updated_at'])
+            db_row = db_service.read_run_for(em.source.owner, em.source.repository)
+
+            def publish_event():
+                print('publishing an update-event.', em.destination.owner, '/', em.destination.repository,
+                      'has changed so invoking',
+                      em.destination.owner, '/', em.destination.repository)
+                github_client.repos().create_repository_dispatch_event(
+                    em.destination.owner, em.destination.repository, 'update-event', '{}')
+                db_service.write_run_for(em.source.owner, em.source.repository, datetime.datetime.now())
 
 
+            if db_row is not None:
+                _, _, _, db_updated_at = db_row
+                if updated_at > db_updated_at:
+                    publish_event()
+            else:
+                publish_event()
