876e9d0877428e40b6a3da9874faab42bba4a167
Josh Long <josh@joshlong.com>
2020-06-09 22:08:10 -0700

    up

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ database. We'd need a PostgreSQL database or something. Or even just a Redis ins
 '''
 
 import datetime
+import logging
 import os
 import sys
 import typing
@@ -23,6 +24,8 @@ import pg8000
 
 import actions
 import mappings
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def build_connection() -> pg8000.Connection:
@@ -42,45 +45,64 @@ def main(_: typing.List[str]):
     github_client = github.SimpleGithubClient(github_token)
     db_service = actions.GithubActionsRunService(build_connection)
 
+    def handle_event_mapping(event_mapping: mappings.EventMapping):
+
+        logging.info('-' * 100)
+        logging.info(
+            f"looking for source.owner {event_mapping.source.owner} and "
+            f"source.repository {event_mapping.source.repository} "
+            f"and workflow file name {event_mapping.source.workflow_file_name}")
+        recent_runs_response = github_client.actions().list_workflow_runs(
+            owner=event_mapping.source.owner,
+            repo=event_mapping.source.repository,
+            workflow_file_name_or_id=event_mapping.source.workflow_file_name,
+            status='success')
+
+        logging.info(recent_runs_response)
+
+        if 'workflow_runs' in recent_runs_response:
+
+            if 'total_count' in recent_runs_response and recent_runs_response['total_count'] == 0:
+                logging.info(f'There are no runs for {event_mapping.source.owner}/{event_mapping.source.repository}. Skipping.')
+                return
+
+            recent_runs = recent_runs_response['workflow_runs']
+            successful_runs = [a for a in recent_runs if
+                               a['status'] == 'completed' and a['conclusion'] == 'success']
+            successful_runs.sort(key=key_generator, reverse=True)
+            latest_successful_run = successful_runs[0]
+            github_updated_at = dateutil.parser.parse(latest_successful_run['updated_at'])
+            db_row = db_service.read_run_for(event_mapping.source.owner, event_mapping.source.repository)
+
+            def publish_event():
+                logging.info('publishing an update-event from', event_mapping.source.owner, '/',
+                             event_mapping.source.repository,
+                             'has changed so invoking', event_mapping.destination.owner, '/',
+                             event_mapping.destination.repository)
+                response = github_client.repositories().create_repository_dispatch_event(
+                    event_mapping.destination.owner, event_mapping.destination.repository, 'update-event',
+                    {'timestamp': datetime.datetime.now().timestamp() * 1000})
+                print(response.status_code)
+                db_service.write_run_for(event_mapping.source.owner, event_mapping.source.repository,
+                                         datetime.datetime.now())
+
+            if db_row is not None:
+                _, _, _, db_updated_at = db_row
+                if github_updated_at > db_updated_at:
+                    publish_event()
+                else:
+                    logging.info('the date is older. no need to run.')
+            else:
+                publish_event()
+
     with open('mappings.json') as json_fp:
         event_mappings = mappings.EventMapping.read_event_mappings(json_fp)
 
         for em in event_mappings:
-
-            recent_runs_response = github_client.actions().list_workflow_runs(
-                owner=em.source.owner,
-                repo=em.source.repository,
-                workflow_file_name_or_id=em.source.workflow_file_name,
-                status='success')
-
-            print(recent_runs_response)
-
-            if 'workflow_runs' in recent_runs_response:
-                recent_runs = recent_runs_response['workflow_runs']
-                successful_runs = [a for a in recent_runs if
-                                   a['status'] == 'completed' and a['conclusion'] == 'success']
-                successful_runs.sort(key=key_generator, reverse=True)
-                latest_successful_run = successful_runs[0]
-                github_updated_at = dateutil.parser.parse(latest_successful_run['updated_at'])
-                db_row = db_service.read_run_for(em.source.owner, em.source.repository)
-
-                def publish_event():
-                    print('publishing an update-event from', em.source.owner, '/', em.source.repository,
-                          'has changed so invoking', em.destination.owner, '/', em.destination.repository)
-                    response = github_client.repositories().create_repository_dispatch_event(
-                        em.destination.owner, em.destination.repository, 'update-event',
-                        {'timestamp': datetime.datetime.now().timestamp() * 1000})
-                    print(response.status_code)
-                    db_service.write_run_for(em.source.owner, em.source.repository, datetime.datetime.now())
-
-                if db_row is not None:
-                    _, _, _, db_updated_at = db_row
-                    if github_updated_at > db_updated_at:
-                        publish_event()
-                    else:
-                        print('the date is older. no need to run.')
-                else:
-                    publish_event()
+            try:
+                handle_event_mapping(em)
+            except Exception as e:
+                logging.error(e)
 
 
 main(sys.argv)
